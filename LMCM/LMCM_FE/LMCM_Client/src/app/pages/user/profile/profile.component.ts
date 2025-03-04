3a30c1a6a0e48f979b028d4930991fc6d49d5187
Quyenlnhe173212 <quyenlnhe173212@fpt.edu.vn>
2025-03-04 17:26:25 +0700

    UserProfile
    FE cho user profile

--- LMCM/LMCM_FE/LMCM_Client/src/app/pages/user/profile/profile.component.ts
+++ LMCM/LMCM_FE/LMCM_Client/src/app/pages/user/profile/profile.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, CardModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  user = {
+    name: 'Nguyễn Văn A',
+    email: 'nguyenvana@example.com',
+    avatar: '' // Đường dẫn ảnh
+  };
 
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.user.avatar = e.target.result; // Hiển thị ảnh đã chọn
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }

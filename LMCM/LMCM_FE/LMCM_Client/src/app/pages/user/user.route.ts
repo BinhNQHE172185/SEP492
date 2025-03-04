2475353e1bd7e9a764c6b11b2fc0b063ea8ba544
hung343 <comradehung@gmail.com>
2025-03-04 21:25:04 +0700

    f

--- LMCM/LMCM_FE/LMCM_Client/src/app/pages/user/user.route.ts
+++ LMCM/LMCM_FE/LMCM_Client/src/app/pages/user/user.route.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { Login } from '../auth/Login/login';
 import { StaffManageComponent } from './staff-manage/staff-manage.component';
 import { ProfileComponent } from './profile/profile.component';
-import { ListSubjectsComponent } from '../list-subjects/list-subjects.component';
+import { ListSubjectsComponent } from './list-subjects/list-subjects.component';
 
 export default [
     { path: 'staff', component: StaffManageComponent },

a910a291099ce263c092f78dd9298d3baa06788a
Nguyễn Quý Bình <binhnqhe172185@fpt.edu.vn>
2025-03-05 15:20:30 +0700

    #10 - List Subject
    refer: #10

---
+++ LMCM/LMCM_BE/DTOs/SubjectDtos/SubjectDto.cs
@@ -0,0 +1,20 @@
+﻿namespace LMCM_BE.DTOs.SubjectDtos
+{
+    public class SubjectViewDto
+    {
+        public Guid SubjectId { get; set; }
+        public string SubjectCode { get; set; } = null!;
+
+        public string SubjectName { get; set; } = null!;
+
+        public string? SubjectNameEnglish { get; set; }
+
+        public bool? IsConstructivist { get; set; }
+
+        public string? Method { get; set; }
+
+        public int? Duration { get; set; }
+
+        public int? Reality { get; set; }
+    }
+}

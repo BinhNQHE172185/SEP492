5dae29139297b9bb92fac1a743b091ad3ed76fbd
Nguyễn Quý Bình <binhnqhe172185@fpt.edu.vn>
2025-03-07 22:36:03 +0700

    Quick FIx
    cứu e quyền

--- LMCM/LMCM_FE/LMCM_Client/src/environments/environment.ts
+++ LMCM/LMCM_FE/LMCM_Client/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:5035/api',
+    google_client_id: '433474498165-m4uv6c6h9hc3ss9vk74d9v7u8t57irr5.apps.googleusercontent.com'
   };
   

d1bd7c7505c7f2d94f5125ff2797aec7bcff53ca
Bachnd23 <bachndhe170904@fpt.edu.vn>
2025-03-15 09:20:59 +0700

    #74 create contract

---
+++ LMCM/LMCM_BE/AutoMapper/ContractProfiles/ContractProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using LMCM_BE.DTOs.ContractDtos;
+using LMCM_BE.Models;
+
+namespace LMCM_BE.AutoMapper.ContractProfiles
+{
+    public class ContractProfile : Profile
+    {
+        public ContractProfile()
+        {
+            CreateMap<ContractInsertDto, Contract>()
+                .ForMember(dest => dest.ContractId, opt => opt.Ignore())
+                .ForMember(dest => dest.Url, opt => opt.Ignore())
+                .ForMember(dest => dest.Status, opt => opt.Ignore()) 
+                .ForMember(dest => dest.CreatedAt, opt => opt.Ignore()) 
+                .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore()); ;
+        }
+    }
+}

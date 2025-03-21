c73d1a46ccf2d8f77eb7030404b740f281ec4550
hung343 <comradehung@gmail.com>
2025-03-21 10:37:26 +0700

    Quản lý hợp đồng - FE #68

--- LMCM/LMCM_FE/LMCM_Client/src/app/pages/docs/docs.route.ts
+++ LMCM/LMCM_FE/LMCM_Client/src/app/pages/docs/docs.route.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ListReportComponent } from '../docs/list-report/list-report.component';
 import { ListExpertComponent } from '../docs/list-expert/list-expert.component';
+import { ListContractComponent } from './list-contract/list-contract.component';
 
 export default [
     { path: 'expert', component: ListExpertComponent },
     { path: 'report', component: ListReportComponent },
+    { path: 'contract', component: ListContractComponent },
 ] as Routes;

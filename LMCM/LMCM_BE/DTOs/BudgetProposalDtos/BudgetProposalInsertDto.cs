547f2acd9014e27f74b294292516a25fcaed5105
Bachnd23 <bachndhe170904@fpt.edu.vn>
2025-03-26 22:06:30 +0700

    #75 validations

--- LMCM/LMCM_BE/DTOs/BudgetProposalDtos/BudgetProposalInsertDto.cs
+++ LMCM/LMCM_BE/DTOs/BudgetProposalDtos/BudgetProposalInsertDto.cs
@@ -1,11 +1,22 @@
-﻿namespace LMCM_BE.DTOs.BudgetProposalDtos
+﻿using System.ComponentModel.DataAnnotations;
+using static LMCM_BE.DTOs.Validators.SharedValidationAtributes;
+
+namespace LMCM_BE.DTOs.BudgetProposalDtos
 {
     public class BudgetProposalInsertDto
     {
+        [Required(ErrorMessage = "Tên tiêu đề là bắt buộc")]
+        [StringLength(100, ErrorMessage = "Độ dài tên tiêu đề không được vượt quá 100 ký tự")]
+        [MinLength(3, ErrorMessage = "Phải có ít nhất 3 ký tự")]
+        [RegularExpression(@"^\s*\S.*$", ErrorMessage = "Tên tiêu đề không được chỉ chứa khoảng trắng")]
         public string Title { get; set; } = null!;
 
+        [DateMustBePresentOrFuture]
         public DateTime? ProposalDate { get; set; }
 
+        [Required(ErrorMessage = "File đính kèm là bắt buộc")]
+        [AllowedFileExtensions(new string[] { ".pdf" }, ErrorMessage = "Chỉ chấp nhận các tệp có định dạng .pdf")]
+        [MaxFileSize(5 * 1024 * 1024, ErrorMessage = "Dung lượng tệp không được vượt quá 5MB")]
         public IFormFile? File { get; set; }
     }
 }

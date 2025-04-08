8e99ee83d2e6ff69c76b11f4a1b68c814ec9a3fc
kimdien189 <diennkhe172193@fpt.edu.vn>
2025-04-08 20:33:56 +0700

    ContractValueItemProfile

---
+++ LMCM/LMCM_BE/AutoMapper/ContractValueItemProfiles/ContractValueItemProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using LMCM_BE.DTOs.ContractValueItemDtos;
+using LMCM_BE.Models;
+
+namespace LMCM_BE.AutoMapper.ContractValueItemProfiles
+{
+    public class ContractValueItemProfile : Profile
+    {
+        public ContractValueItemProfile()
+        {
+            CreateMap<ContractValueItemDto, ContractValueItem>();
+
+            CreateMap<ContractValueItem, ContractValueItemDto>();
+        }
+    }
+}

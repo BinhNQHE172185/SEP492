be21d93695c5ac3f6a199645db33cd09c8ba66f2
kimdien189 <diennkhe172193@fpt.edu.vn>
2025-04-09 12:31:19 +0700

    seedata user account logic

---
+++ LMCM/LMCM_BE/Utilities/DefaultUsers.cs
@@ -0,0 +1,17 @@
+﻿namespace LMCM_BE.Utilities
+{
+    public class DefaultUsers
+    {
+        public static readonly List<string> HeadOfDepartmentEmails = new()
+            {
+                "binhnqhe172185@fpt.edu.vn",
+                "diennkhe172193@fpt.edu.vn",
+                "bachndhe170904@fpt.edu.vn"
+            };
+        public static readonly List<string> Roles = new()
+            {
+                "Head of Department",
+                "Staff"
+            };
+    }
+}

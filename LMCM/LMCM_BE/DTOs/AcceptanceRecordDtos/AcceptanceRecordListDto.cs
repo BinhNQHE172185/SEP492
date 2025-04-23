e1a37e3ec07a9e5f8cc6510971818f8ee0ef0f30
Bachnd23 <bachndhe170904@fpt.edu.vn>
2025-04-23 18:24:26 +0700

    fix

--- LMCM/LMCM_BE/DTOs/AcceptanceRecordDtos/AcceptanceRecordListDto.cs
+++ LMCM/LMCM_BE/DTOs/AcceptanceRecordDtos/AcceptanceRecordListDto.cs
@@ -1,16 +1,14 @@
-﻿using LMCM_BE.DTOs.ContractDtos;
-
-namespace LMCM_BE.DTOs.AcceptanceRecordDtos
+﻿namespace LMCM_BE.DTOs.AcceptanceRecordDtos
 {
     public class AcceptanceRecordListDto
     {
         public Guid AcceptanceId { get; set; }
-        public Guid ContractId { get; set; }
         public string Title { get; set; } = null!;
 
         public DateTime? AcceptanceDate { get; set; }
 
         public decimal? FinalPrice { get; set; }
-        public virtual ContractDetailDto Contract { get; set; } = null!;
+        public Guid? contractId { get; set; }
+        public string? contractTitle { get; set; }
     }
 }

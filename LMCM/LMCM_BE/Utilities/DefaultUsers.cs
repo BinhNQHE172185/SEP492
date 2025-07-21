cc2907ee114ced1ea8cad2d8147b8f7c0aeea34a
hung343 <comradehung@gmail.com>
2025-07-21 09:21:18 +0700

    h

--- LMCM/LMCM_BE/Utilities/DefaultUsers.cs
+++ LMCM/LMCM_BE/Utilities/DefaultUsers.cs
@@ -5,6 +5,7 @@
         public static readonly List<string> HeadOfDepartmentEmails = new()
             {
                 "binhnqhe172185@fpt.edu.vn",
+                "hungtqhe172421@fpt.edu.vn",
                 "diennkhe172193@fpt.edu.vn",
                 "bachndhe170904@fpt.edu.vn"
             };
